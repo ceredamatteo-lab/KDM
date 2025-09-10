e55446ae75e53736e408bce38258448732237e44
tommaso <tbecchi@Matteos-MacBook-Air.local>
2025-09-10 17:39:12 +0200

    KDM profiles script [TB]

--- Scripts/figureB.R
+++ Scripts/figureB.R
@@ -25,13 +25,17 @@ center <- function(s, l) {
   return(s[start:end])
 }
 
-bdir<-"/tmp/tempres/"
+bdir<-"/tmp/tempres2/"
 #system("mkdir /tmp/tempres/")
 
 exp="ENCSR001KKZ"
 u=which(exp_info$Experiment==exp)
 
 #=1
+total=rbind()
+for(u in 1:nrow(exp_info)){
+  print(u)
+exp=exp_info$Experiment[u]
 train0=DATASET[[u]]$train
 trainLabels=DATASET[[u]]$trainLabels
 
@@ -51,9 +55,13 @@ system(cmd_centrimo,intern=TRUE,ignore.stderr=TRUE)
 centrimo<-read_centrimo(paste(bdir,"centrimo/centrimo.tsv",sep=""))
 
 all_selected_motifs<-centrimo$motif_id[which(centrimo$E.value<0.05 & centrimo$bin_location<=0)]
+if(length(all_selected_motifs)==0){next}
 top_centrimo=all_selected_motifs[1:min(length(all_selected_motifs),5)]
 
-pn=kdmGetProfileInfo(motifs = mcross_W,regions = train0,
+pp=which(colnames(mcross_W)%in%top_centrimo)
+new_W=mcross_W[,pp]
+
+pn=kdmGetProfileInfo(motifs = new_W,regions = train0,
     centers = rep(36, nrow(DATASET[[u]]$train)),
     labels = trainLabels,
     genomeFile = "/adat/Progetti/RBP/RNA_exons/Reference/hg38/hg38.2bit",
@@ -66,7 +74,7 @@ pn=kdmGetProfileInfo(motifs = mcross_W,regions = train0,
 
 ne<-kdmCentrimo(pn,symmetric=FALSE,tail="upper")
 res<-c()
-for(k in 1:ncol(mcross_W)){
+for(k in 1:ncol(new_W)){
    E<-ne[[k]]
    cpos<-pn$x[2:(length(pn$x)-1)][floor((E$Start+E$End)/2)]
    cond<-which(cpos<=0 & (E$Centrality_q.value * ncol(mcross_W)) < 0.05 & (E$Enrichment_q.value  * ncol(mcross_W))<0.05 & E$Centrality>1 & E$Enrichment>1)
@@ -75,6 +83,19 @@ for(k in 1:ncol(mcross_W)){
      res<-rbind(res,data.frame(motif=k,ss[1,]))
    }
 }
+
+if(is.null(res)){
+  total=rbind(total,data.frame(Experiment=exp,Motif=top_centrimo,Selected=FALSE))
+  next
+}
+
+total=rbind(total,data.frame(Experiment=exp,Motif=top_centrimo,Selected=top_centrimo%in%colnames(new_W)[res$motif]))
+}
+
+
+
+
+
 
 selected_kdm=c()
 if(!is.null(res)){
